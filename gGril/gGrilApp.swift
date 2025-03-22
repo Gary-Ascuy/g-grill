a0646a93197084d2b7312fdb85a575e207ec444b
Gary Ascuy <gary.ascuy@gmail.com>
2025-03-22 10:05:05 -0500

    Initial Commit

---
+++ gGril/gGrilApp.swift
@@ -0,0 +1,17 @@
+//
+//  gGrilApp.swift
+//  gGril
+//
+//  Created by Gary Ascuy on 22/03/25.
+//
+
+import SwiftUI
+
+@main
+struct gGrilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gGrilTests/gGrilTests.swift
@@ -0,0 +1,17 @@
+//
+//  gGrilTests.swift
+//  gGrilTests
+//
+//  Created by Gary Ascuy on 22/03/25.
+//
+
+import Testing
+@testable import gGril
+
+struct gGrilTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
