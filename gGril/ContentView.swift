a6dfc510ea85a0c28b4ddd8587e729cfcd14837f
Gary Ascuy <gary.ascuy@gmail.com>
2025-03-25 00:15:54 -0500

    Adding buttons and menu

--- gGril/ContentView.swift
+++ gGril/ContentView.swift
@@ -8,14 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showCat = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "cat")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, Gary!")
+        NavigationStack {
+            VStack(alignment: .center) {
+                Spacer()
+                Button(action: {
+                    print("Cat was clicked")
+                    showCat = true
+                }) {
+                    Label("", systemImage: "cat")
+                        .font(.system(size: 100))
+                }
+                .disabled(showCat)
+                Spacer()
+                Divider()
+                Spacer()
+                Button(action: {
+                    print("Dog was clicked")
+                    showCat = false
+                }) {
+                    Label("", systemImage: "dog")
+                        .font(.system(size: 100))
+                }
+                .disabled(!showCat)
+                Spacer()
+            }
+        }.padding()
+        .toolbar {
+            ToolbarItem(placement: .bottomBar) {
+                HStack(alignment: .center){
+                    Button(action: {
+                        print("Cat was clicked")
+                    }) {
+                        Label("Cat Button", systemImage: "snowflake")
+                    }
+                    .disabled(showCat)
+                    Spacer()
+                    Button(action: {
+                        print("Cat was clicked")
+                    }) {
+                        Label("Cat Button", systemImage: "gear")
+                    }
+                    .disabled(showCat)
+                    Spacer()
+                    Button(action: {
+                        print("Cat was clicked")
+                    }) {
+                        Label("Cat Button", systemImage: "house")
+                    }
+                    .disabled(showCat)
+                    Spacer()
+                    Button(action: {
+                        print("Cat was clicked")
+                    }) {
+                        Label("Cat Button", systemImage: "bell.badge")
+                    }
+                    .disabled(showCat)
+                    Spacer()
+                    Button(action: {
+                        print("Cat was clicked")
+                    }) {
+                        Label("Cat Button", systemImage: "square.and.arrow.up")
+                    }
+                    .disabled(showCat)
+                }
+                
+            }
         }
-        .padding()
     }
 }
 
